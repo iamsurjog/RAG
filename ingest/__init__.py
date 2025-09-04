d3776ac446f338a21688922119e4e4428bb3b66f
iamsurjog <iamsurjog@gmail.com>
2025-09-04 18:35:20 +0530

    feat: added support for md files

--- ingest/__init__.py
+++ ingest/__init__.py
@@ -3,7 +3,7 @@ from ingest import txt, pdf
 def read(fpath):
     # print("helo")
     ext = fpath.split(".")[-1]
-    if ext == "txt":
+    if ext in ["txt", "md"]:
         return txt.read(fpath)
     if ext == "pdf":
         return pdf.read(fpath)
