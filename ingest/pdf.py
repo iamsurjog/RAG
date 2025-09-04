438b6096ea7f3ccbb2ec5d2d38b185086d1029ca
iamsurjog <iamsurjog@gmail.com>
2025-09-04 18:35:42 +0530

    fix: made the pdf text splitter be more consistent with the other format types

--- ingest/pdf.py
+++ ingest/pdf.py
@@ -4,5 +4,5 @@ def read(file):
     reader = PdfReader(file)
     texts = []
     for page in reader.pages:
-        texts.append(page.extract_text())
+        texts.extend(page.extract_text().split("\n"))
     return texts
